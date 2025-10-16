2a0df34c9464333b029790ac638f4d28c8f4fa65
adel elsawy <adelelsawy032@gmail.com>
2025-10-16 19:41:39 +0300

    update base

--- Routes/base.py
+++ Routes/base.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI , APIRouter
+import os
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix ="/API/V1"
+   , tags = ["api_v1"])
+
 
 @base_router.get("/")
 def welcome_back():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+
     return {
-        "message" : "hello yyy" }
+        "app_name": app_name
+        , "app_version":app_version
+        }
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+
 from Routes import base
+
 
 
 app = FastAPI()
